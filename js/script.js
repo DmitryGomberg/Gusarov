3ff8b3131196cd7a3c993c2f015664289a8fd59f
Dmitry <dpgomberg@gmail.com>
2021-07-24 19:22:26 +0300

    ываываыа

--- js/script.js
+++ js/script.js
@@ -45,6 +45,98 @@ $(document).ready(function () {
       $(".tab5_item").hide().eq($(this).index()).fadeIn();
    }).eq(0).addClass("active");
 
+
+   var currentTab = 0;
+   $('.s-s14-button-wrap').addClass('o');
+   $(".s-s14-item").removeClass('active');
+   $(".s-s14-step").removeClass('active');
+   $(".s-s14-item").eq(currentTab).addClass('active');
+   $(".s-s14-step").eq(currentTab).addClass('active');
+   if ($(document).width() < '991') {
+      $(".s-s14-button-next").click(function () {
+         currentTab++;
+         console.log(currentTab);
+         $('.s-s14-button-wrap').addClass();
+         if (currentTab == 0) {
+            $('.s-s14-button-wrap').addClass('o');
+            $('.s-s14-button-wrap').removeClass('l');
+            $('.s-s14-button-wrap').removeClass('s');
+         }
+         if (currentTab == 1) {
+            $('.s-s14-button-wrap').addClass('l');
+            $('.s-s14-button-wrap').removeClass('o');
+            $('.s-s14-button-wrap').removeClass('s');
+         }
+         if (currentTab == 2) {
+            $('.s-s14-button-wrap').removeClass('o');
+            $('.s-s14-button-wrap').removeClass('l');
+            $('.s-s14-button-wrap').addClass('s');
+            $(".s-s14-next-step-img").addClass('disable');
+            $(".s-s14-button-submit").addClass('active');
+         }
+         console.log(currentTab);
+         $(".s-s14-item").removeClass('active');
+         $(".s-s14-step").removeClass('active');
+         $(".s-s14-item").eq(currentTab).addClass('active');
+         $(".s-s14-step").eq(currentTab).addClass('active');
+      });
+      $(".s-s14-button-before").click(function () {
+         currentTab--;
+         console.log(currentTab);
+         $('.s-s14-button-wrap').addClass();
+         if (currentTab == 0) {
+            $('.s-s14-button-wrap').addClass('o');
+            $('.s-s14-button-wrap').removeClass('l');
+            $('.s-s14-button-wrap').removeClass('s');
+         }
+         if (currentTab == 1) {
+            $('.s-s14-button-wrap').addClass('l');
+            $('.s-s14-button-wrap').removeClass('o');
+            $('.s-s14-button-wrap').removeClass('s');
+         }
+         if (currentTab == 2) {
+            $('.s-s14-button-wrap').removeClass('o');
+            $('.s-s14-button-wrap').removeClass('l');
+            $('.s-s14-button-wrap').addClass('s');
+            $(".s-s14-next-step-img").addClass('disable');
+            $(".s-s14-button-submit").addClass('active');
+         }
+         console.log(currentTab);
+         $(".s-s14-item").removeClass('active');
+         $(".s-s14-step").removeClass('active');
+         $(".s-s14-item").eq(currentTab).addClass('active');
+         $(".s-s14-step").eq(currentTab).addClass('active');
+      })
+   }
+   $(".s-s14-next-step-img").click(function () {
+      currentTab++;
+      console.log(currentTab);
+      $('.s-s14-button-wrap').addClass();
+      if (currentTab == 0) {
+         $('.s-s14-button-wrap').addClass('o');
+         $('.s-s14-button-wrap').removeClass('l');
+         $('.s-s14-button-wrap').removeClass('s');
+      }
+      if (currentTab == 1) {
+         $('.s-s14-button-wrap').addClass('l');
+         $('.s-s14-button-wrap').removeClass('o');
+         $('.s-s14-button-wrap').removeClass('s');
+      }
+      if (currentTab == 2) {
+         $('.s-s14-button-wrap').removeClass('o');
+         $('.s-s14-button-wrap').removeClass('l');
+         $('.s-s14-button-wrap').addClass('s');
+         $(".s-s14-next-step-img").addClass('disable');
+         $(".s-s14-button-submit").addClass('active');
+      }
+      console.log(currentTab);
+      $(".s-s14-item").removeClass('active');
+      $(".s-s14-step").removeClass('active');
+      $(".s-s14-item").eq(currentTab).addClass('active');
+      $(".s-s14-step").eq(currentTab).addClass('active');
+   })
+
+
    if ($(document).width() > '991') {
       // Scrollbar.initAll();
       $(".team-img").hover(function () {
@@ -202,3 +294,83 @@ $(document).ready(function () {
       event.preventDefault();
    });
 });
+
+var x, i, j, l, ll, selElmnt, a, b, c;
+/*look for any elements with the class "custom-select":*/
+x = document.getElementsByClassName("custom-select");
+l = x.length;
+for (i = 0; i < l; i++) {
+   selElmnt = x[i].getElementsByTagName("select")[0];
+   ll = selElmnt.length;
+   /*for each element, create a new DIV that will act as the selected item:*/
+   a = document.createElement("DIV");
+   a.setAttribute("class", "select-selected");
+   a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+   x[i].appendChild(a);
+   /*for each element, create a new DIV that will contain the option list:*/
+   b = document.createElement("DIV");
+   b.setAttribute("class", "select-items select-hide");
+   for (j = 1; j < ll; j++) {
+      /*for each option in the original select element,
+      create a new DIV that will act as an option item:*/
+      c = document.createElement("DIV");
+      c.innerHTML = selElmnt.options[j].innerHTML;
+      c.addEventListener("click", function (e) {
+         /*when an item is clicked, update the original select box,
+         and the selected item:*/
+         var y, i, k, s, h, sl, yl;
+         s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+         sl = s.length;
+         h = this.parentNode.previousSibling;
+         for (i = 0; i < sl; i++) {
+            if (s.options[i].innerHTML == this.innerHTML) {
+               s.selectedIndex = i;
+               h.innerHTML = this.innerHTML;
+               y = this.parentNode.getElementsByClassName("same-as-selected");
+               yl = y.length;
+               for (k = 0; k < yl; k++) {
+                  y[k].removeAttribute("class");
+               }
+               this.setAttribute("class", "same-as-selected");
+               break;
+            }
+         }
+         h.click();
+      });
+      b.appendChild(c);
+   }
+   x[i].appendChild(b);
+   a.addEventListener("click", function (e) {
+      /*when the select box is clicked, close any other select boxes,
+      and open/close the current select box:*/
+      e.stopPropagation();
+      closeAllSelect(this);
+      this.nextSibling.classList.toggle("select-hide");
+      this.classList.toggle("select-arrow-active");
+   });
+}
+
+function closeAllSelect(elmnt) {
+   /*a function that will close all select boxes in the document,
+   except the current select box:*/
+   var x, y, i, xl, yl, arrNo = [];
+   x = document.getElementsByClassName("select-items");
+   y = document.getElementsByClassName("select-selected");
+   xl = x.length;
+   yl = y.length;
+   for (i = 0; i < yl; i++) {
+      if (elmnt == y[i]) {
+         arrNo.push(i)
+      } else {
+         y[i].classList.remove("select-arrow-active");
+      }
+   }
+   for (i = 0; i < xl; i++) {
+      if (arrNo.indexOf(i)) {
+         x[i].classList.add("select-hide");
+      }
+   }
+}
+/*if the user clicks anywhere outside the select box,
+then close all select boxes:*/
+document.addEventListener("click", closeAllSelect);
